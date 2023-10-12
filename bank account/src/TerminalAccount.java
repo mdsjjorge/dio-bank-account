da2b652b2868c8bb641843851871428eb764d64d
mdsjjorge <mdsousa.jorge@gmail.com>
2023-10-12 16:09:31 -0300

    feat(account): main class account working

--- bank account/src/TerminalAccount.java
+++ bank account/src/TerminalAccount.java
@@ -1,5 +1,27 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TerminalAccount {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter the account number: ");
+        int account = scanner.nextInt();
+
+        System.out.println("Please, enter the agency number: ");
+        String agency = scanner.next();
+
+        System.out.println("Please, enter your name: ");
+        String name = scanner.next();
+
+        System.out.println("Great, " + name + "! To finish, enter your balance: ");
+        double balance = scanner.nextDouble();
+        NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        String formatBalance = format.format(balance);
+
+        System.out.println("Very good, " + name + "! Thank you for creating an account with our bank. " +
+                " Your agency is " + agency + ", account " + account + " and your balance " + formatBalance +
+                " is now available for withdraw. ");
     }
 }
